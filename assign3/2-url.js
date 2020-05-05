073c726df433511b9c77cd605ddb6b6d33be798e
Jennie <ljennie@pdx.edu>
2020-05-05 13:41:55 -0700

    updating 2-url.js in-progress

--- assign3/2-url.js
+++ assign3/2-url.js
@@ -1,5 +1,11 @@
 // Enter your code here
 
+function url_parsing(){
+    var url = document.getElementById("comments").value;
+
+    document.write(url);
+}
+
 // Input:
 // http://www.example.com?name=r2d2&email=r2d2%40me.com&human=no
 
