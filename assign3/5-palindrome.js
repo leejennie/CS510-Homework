8996cf031a326c50af65fdc9f833f9c5f4f73bb7
Jennie <ljennie@pdx.edu>
2020-05-07 16:26:36 -0700

    updating 5-palindrome.js complete

--- assign3/5-palindrome.js
+++ assign3/5-palindrome.js
@@ -1,1 +1,22 @@
 // Enter your code here
+
+function check(event) {
+
+    event.preventDefault();
+
+    var input = document.getElementById("number").value;
+    var div = document.getElementById("result");
+
+    var reverse = input.split('').reverse().join('');
+
+    if (reverse == input) {
+        var p = document.createElement('p');
+        p.textContent = input + " is a palindrome"; 
+        div.appendChild(p);
+    }
+    else {
+        var p = document.createElement('p');
+        p.textContent = input + " is not a palindrome"; 
+        div.appendChild(p);
+    }
+}
