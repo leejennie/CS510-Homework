764763938e504cf32a2a80ef6e7936d99b63d397
Jennie <ljennie@pdx.edu>
2020-05-07 15:15:54 -0700

    updating 4-regex.js complete

--- assign3/4-regex.js
+++ assign3/4-regex.js
@@ -1,1 +1,20 @@
 // Enter your code here
+
+function separate(event) {
+
+    event.preventDefault();
+
+    var text = document.getElementById("userinput").value;
+    
+    var store = text.split(/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g);
+   
+    var div = document.getElementById("results");
+
+    for (var i = 0; i < store.length; i++) {
+        var p = document.createElement('p');
+        p.setAttribute('class', 'phrase');
+        p.textContent = store[i];
+        div.appendChild(p); 
+    }
+    
+}
