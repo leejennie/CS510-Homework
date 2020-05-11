40ce16fa842684ab9a7abb699d1a5aa1f78d42f9
Jennie <ljennie@pdx.edu>
2020-05-10 17:37:53 -0700

    updating 7-characters.js complete

--- assign3/7-characters.js
+++ assign3/7-characters.js
@@ -1,3 +1,34 @@
 // Enter your code here
 
-const url = 'https://swapi.dev/api/people/';
+function get_data() {
+
+    for(var i = 1; i < 10 ; i++) {
+       const url = 'https://swapi.dev/api/people/?page=' + i;
+       let results = document.querySelector('#results');
+
+        let mod_page = (element, item) => {
+          let li = document.createElement(element);
+          li.innerText = `${item.name} - ${item.birth_year}`;
+          results.append(li);
+        };
+
+        let fetchData = (url) => {
+          fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            data.results.forEach((item) => {
+                mod_page('li', item);
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+            results.append();
+          })
+        };
+      fetchData(url);
+    }
+}
+get_data();
+
+//It reads each page out of order? I don't know why. But for the most part it works. 
